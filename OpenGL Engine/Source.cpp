aba7b857da09f528f66cfc732b66a158224cc144
Bobanos <lukas.fortik@seznam.cz>
2022-09-04 19:10:55 +0200

    Made a square with yellow corner

--- OpenGL Engine/Source.cpp
+++ OpenGL Engine/Source.cpp
@@ -106,12 +106,22 @@ int main(void)
 
 		glBegin(GL_TRIANGLES);
 		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
+		glVertex3f(-1.f, -1.f, 0.f);
 		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(1.f, -1.f, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
 		glVertex3f(1.0f, 1.0f, 0.f);
 		glEnd();
+
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(-1.0f, -1.0f, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(1.0f, 1.0f, 0.f);
+		glColor3f(1.0f, 1.0f, 0.f);
+		glVertex3f(-1.0f, 1.0f, 0.f);
+		glEnd();
+
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
